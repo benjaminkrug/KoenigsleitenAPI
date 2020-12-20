0d7220d08e66c0f83c083eaa6498da48438c6be3
benni <krug.benjamin.92>
2020-12-20 15:59:58 +0100

    init

---
+++ KoenigsleitenAPI/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿namespace KoenigsleitenAPI.Controllers
+{
+    using IdentityModel.Client;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            //var accessToken = await HttpContext.GetTokenAsync("access_token");
+            //var idToken = await HttpContext.GetTokenAsync("id_token");
+            //var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+
+            //var claims = User.Claims.ToList();
+            //var _idToken = new JwtSecurityTokenHandler().ReadJwtToken(idToken);
+            //var _accessToken = new JwtSecurityTokenHandler().ReadJwtToken(accessToken);
+
+            //var result = await GetSectret(accessToken);
+            //await RefreshAccessToken();
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookie", "oidc");
+        }
+
+        public async Task<string> GetSectret(string accessToken)
+        {
+            var apiClient = _httpClientFactory.CreateClient();
+
+            apiClient.SetBearerToken(accessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44309/secret");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+
+        public async Task RefreshAccessToken()
+        {
+            var serverClient = _httpClientFactory.CreateClient();
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync("https://localhost:44354/");
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+            var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+            var refreshTokenClient = _httpClientFactory.CreateClient();
+
+            var tokenResponse = await refreshTokenClient.RequestRefreshTokenAsync(new RefreshTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                RefreshToken = refreshToken,
+                ClientId = "client_id_mvc",
+                ClientSecret = "client_secret_mvc"
+            });
+
+            var authInfo = await HttpContext.AuthenticateAsync("Cookie");
+
+            authInfo.Properties.UpdateTokenValue("access_token", tokenResponse.AccessToken);
+            authInfo.Properties.UpdateTokenValue("id_token", tokenResponse.IdentityToken);
+            authInfo.Properties.UpdateTokenValue("refresh_token", tokenResponse.RefreshToken);
+
+            await HttpContext.SignInAsync("Cookie", authInfo.Principal, authInfo.Properties);
+
+            var accessTokenDifferent = !accessToken.Equals(tokenResponse.AccessToken);
+            var idTokenDifferent = !idToken.Equals(tokenResponse.IdentityToken);
+            var refreshTokenDifferent = !refreshToken.Equals(tokenResponse.RefreshToken);
+
+
+
+        }
+    }
+}
---
+++ KoenigsleitenAPI/Startup.cs
@@ -0,0 +1,82 @@
+namespace KoenigsleitenAPI
+{
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(config =>
+            {
+                config.DefaultScheme = "Cookie";
+                config.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookie")
+                .AddOpenIdConnect("oidc", config =>
+                {
+                    config.Authority = "https://identityserverkoenigsleiten-dev-as.azurewebsites.net/";
+                    config.ClientId = "client_id_mvc";
+                    config.ClientSecret = "client_secret_mvc";
+                    config.SaveTokens = true;
+                    config.ResponseType = "code";
+                    config.SignedOutCallbackPath = "/Home/Index";
+
+                    // configure cookie claim mapping
+                    config.ClaimActions.DeleteClaim("amr");
+                    config.ClaimActions.MapUniqueJsonKey("RawCoding.Grandma", "rc.grandma");
+
+                    // two trips to load claims in to the cookie
+                    config.GetClaimsFromUserInfoEndpoint = true;
+
+                    // configure scope
+                    config.Scope.Clear();
+                    config.Scope.Add("openid");
+                    config.Scope.Add("rc.scope");
+                    config.Scope.Add("ApiOne");
+                    config.Scope.Add("ApiTwo");
+                    config.Scope.Add("offline_access");
+
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
